2028a7282c697bb1e33f18643147ea5a1c51eb63
suxaylshokirov <suxaylshokirov@gmail.com>
2024-12-19 14:54:08 +0500

    for learning purposes

---
+++ test.py
@@ -0,0 +1,10 @@
+print("I understand it now...")
+
+"""
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/suxaylshokirov/white-ferrari.git
+git push -u origin main
+"""
